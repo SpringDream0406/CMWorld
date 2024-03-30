de8688843066c1d41768f0d441c7ea967cd3093e
SpringDream0406 <SpringDream0406@gmail.com>
2024-03-30 13:00:36 +0900

    v1.0.0

---
+++ src/pages/home/Home_left.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Home_left() {
+  return (
+    <div>
+        <div className='wrapper__left__header'>
+
+        </div>
+        <div className='wrapper__body'>
+          
+        </div>
+    </div>
+  )
+}
+
+export default Home_left
