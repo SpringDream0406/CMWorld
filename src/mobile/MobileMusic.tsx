52f2bece189c3d1e9ea2adeddb30bd8d5135c812
SpringDream0406 <springdream0406@gmail.com>
2024-05-23 13:29:09 +0900

    유튭 테스트

--- src/mobile/MobileMusic.tsx
+++ src/mobile/MobileMusic.tsx
@@ -1,3 +1,4 @@
+import ReactPlayer from "react-player";
 import "./MobileMusic.css";
 
 const MobileMusic = () => {
@@ -6,35 +7,51 @@ const MobileMusic = () => {
       <div className="top">
         <div className="playing-list">plaing</div>
         <div className="cm-music">CM Music</div>
-        <div className="volume">volume</div>
+        <div className="volume">
+          <input
+            type="range"
+            min="0"
+            max="100"
+            defaultValue="30"
+            step="1"
+            className="volumerange"
+          />
+        </div>
       </div>
       <div className="body">
         <div className="song-info">
-          <div className="img">이미지</div>
+          <div className="img">
+            <ReactPlayer
+              url="https://www.youtube.com/watch?v=ZlJNkflJjA4"
+              controls={false}
+              width={"100%"}
+              height={"100%"}
+            />
+          </div>
           <div className="title">
             Rise fea. The Glitch Mob & Mako & The Word Alive
           </div>
-          <div className="artist">가수</div>
+          <div className="artist">Duke & Jones & Louis Therou</div>
         </div>
         <div className="controls">
-          <div className="play-bar">play-bar</div>
-          <div className="btns">btn</div>
+          <div className="play-bar">
+            <div className="time-start">00:00</div>
+            <input
+              type="range"
+              min="0"
+              max="100"
+              defaultValue="0"
+              step="1"
+              className="play-bar-range"
+            />
+            <div className="time-end">04:30</div>
+          </div>
+          <div className="btns">ㅁ ㅇ ㄴ </div>
         </div>
       </div>
       <div className="footer">
         <div className="info">모바일의 경우 뮤직플레이어만 지원합니다.</div>
       </div>
-      {/* <div className="volumebg">
-        <span className="bar"></span>
-        <input
-          type="range"
-          min="0"
-          max="100"
-          defaultValue="50"
-          step="1"
-          className="volumerange"
-        />
-      </div> */}
     </div>
   );
 };
