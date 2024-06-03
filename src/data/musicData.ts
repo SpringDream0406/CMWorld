d181f28fcd656c07e9036a68041d2edbf6ea1f82
SpringDream0406 <springdream0406@gmail.com>
2024-06-03 21:30:21 +0900

    a

--- src/data/musicData.ts
+++ src/data/musicData.ts
@@ -38,6 +38,12 @@ export const musicData: IMusicData[] = [
   //   title: "",
   //   playlists: [pl.Pick2024],
   // },
+  // {
+  //   url: "",
+  //   artist: "",
+  //   title: "",
+  //   playlists: [pl.Pick2024],
+  // },
   {
     url: "https://youtu.be/HpAqkHvu_FE?si=Q2IhgJPJPsa6pCib",
     artist: "폴킴",
